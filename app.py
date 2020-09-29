499988843fa213d02e96191df10844a27d85293f
Phinamihayo <JosephineMihayo@gmail.co>
2020-09-29 18:56:01 +0300

    I create a index.html but after i try to navigate http://127.0.0.1:5000/ it refuse to connect

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FlaskBlog</title>
+</head>
+<body>
+   <h1>Welcome to FlaskBlog</h1>
+</body>
+</html>
+
