ce972cde0b5400226607b0287a5d1489dc32ae51
Phinamihayo <JosephineMihayo@gmail.co>
2020-10-04 22:39:02 +0300

    I add a secret key  code on app.py

--- app.py
+++ app.py
@@ -70,3 +70,22 @@ def get_post(post_id):
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+  import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+. . .  . . .
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+. . .
+
