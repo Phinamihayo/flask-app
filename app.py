c26efa8a99c755cfbd2581ec8acaa076451b63bb
Phinamihayo <JosephineMihayo@gmail.co>
2020-10-09 22:57:40 +0300

    now i understand about some errors i was being getting

--- app.py
+++ app.py
@@ -7,90 +7,84 @@ def index():
     return render_template('index.html')
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>FlaskBlog</title>
-</head>
-<body>
-   <h1>Welcome to FlaskBlog</h1>
-</body>
-</html>
 
-import sqlite3
-from flask import Flask, render_template
+# '''
+# import sqlite3
+# from flask import Flask, render_template
+# from flask import Flask, render_template
+
+# def get_db_connection():
+#     conn = sqlite3.connect('database.db')
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
 
-from flask import Flask, render_template
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-. . .
-
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+# #@app.route('/')
+# def index():
+#     conn = get_db_connection()
+#     posts = conn.execute('SELECT * FROM posts').fetchall()
+#     conn.close()
+#     return render_template('index.html', posts=posts)
 
 
-import sqlite3
-from flask import Flask, render_template
-from werkzeug.exceptions import abort
+# import sqlite3
+# from flask import Flask, render_template
+# from werkzeug.exceptions import abort
+# '''
 
-. . .
-
-. . .
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
-. . .
-
-. . .
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
-  import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
-
-. . .  . . .
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+# '''
+# def get_post(post_id):
+#     conn = get_db_connection()
+#     post = conn.execute('SELECT * FROM posts WHERE id = ?',
+#                         (post_id,)).fetchone()
+#     conn.close()
+#     if post is None:
+#         abort(404)
+#     return post
 
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
 
-. . .
 
-. . .
+# @app.route('/<int:post_id>')
+# def post(post_id):
+#     post = get_post(post_id)
+#     return render_template('post.html', post=post)
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    return render_template('create.html')
+
+
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'your secret key'
+
+
+# @app.route('/')
+# def index():
+#     conn = get_db_connection()
+#     posts = conn.execute('SELECT * FROM posts').fetchall()
+#     conn.close()
+#     return render_template('index.html', posts=posts)
+
+
+# #@app.route('/create', methods=('GET', 'POST'))
+# #def create():
+# #    return render_template('create.html')
+
+    
+# @app.route('/create', methods=('GET', 'POST'))
+# def create():
+#     if request.method == 'POST':
+#         title = request.form['title']
+#         content = request.form['content']
+
+#         if not title:
+#             flash('Title is required!')
+#         else:
+#             conn = get_db_connection()
+#             conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+#                          (title, content))
+#             conn.commit()
+#             conn.close()
+#             return redirect(url_for('index'))
+
+#     return render_template('create.html')
