fdf306a9f4b48de9837ae5e906fd691efdd809d2
Phinamihayo <JosephineMihayo@gmail.co>
2020-10-11 17:57:33 +0300

    I add code app.py

--- app.py
+++ app.py
@@ -66,28 +66,28 @@ def index():
 #     return render_template('index.html', posts=posts)
 
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-     return render_template('create.html')
-
-    
 # @app.route('/create', methods=('GET', 'POST'))
 # def create():
-#     if request.method == 'POST':
-#          title = request.form['title']
-#          content = request.form['content']
+#      return render_template('create.html')
 
-#          if not title:
-#              flash('Title is required!')
-#          else:
-#              conn = get_db_connection()
-#              conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-#                           (title, content))
-#              conn.commit()
-#              conn.close()
-#              return redirect(url_for('index'))
+    
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+     if request.method == 'POST':
+          title = request.form['title']
+          content = request.form['content']
 
-#     return render_template('create.html')
+          if not title:
+              flash('Title is required!')
+          else:
+              conn = get_db_connection()
+              conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                           (title, content))
+              conn.commit()
+              conn.close()
+              return redirect(url_for('index'))
+
+     return render_template('create.html')
 
 
 
