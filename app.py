5452ae4e1e40cda4cc8cfb2cb2475045d64667d7
Phinamihayo <JosephineMihayo@gmail.co>
2020-10-15 17:11:01 +0300

    I was getting an error when i navigate but i tried to import request and it's workout

--- app.py
+++ app.py
@@ -13,10 +13,15 @@ def index():
 # from flask import Flask, render_template
 # from flask import Flask, render_template
 
-# def get_db_connection():
-#     conn = sqlite3.connect('database.db')
-#     conn.row_factory = sqlite3.Row
-#     return conn
+
+from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 
 
 
@@ -68,26 +73,29 @@ def index():
 
 # @app.route('/create', methods=('GET', 'POST'))
 # def create():
-#      return render_template('create.html')
+#     return render_template('create.html')
 
-    
-@app.route('/create', methods=('GET', 'POST'))
-def create():
+
+from flask import request
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+     post = get_post(id)
+
      if request.method == 'POST':
-          title = request.form['title']
-          content = request.form['content']
+         title = request.form['title']
+         content = request.form['content']
 
-          if not title:
-              flash('Title is required!')
-          else:
-              conn = get_db_connection()
-              conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                           (title, content))
-              conn.commit()
-              conn.close()
-              return redirect(url_for('index'))
+         if not title:
+             flash('Title is required!')
+         else:
+             conn = get_db_connection()
+             conn.execute('UPDATE posts SET title = ?, content = ?'
+                          ' WHERE id = ?',
+                          (title, content, id))
+             conn.commit()
+             conn.close()
+             return redirect(url_for('index'))
 
-     return render_template('create.html')
-
-
+     return render_template('edit.html', post=post)
 
